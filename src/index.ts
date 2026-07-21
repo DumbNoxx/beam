c8eac04208e1127aa872addd1bb4c030fd3416e8
Nxus <nxusdev@protonmail.com>
2026-07-21 00:27:56 -0400

    feat: add heartbeat homelab

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,27 @@ import { Hono } from 'hono'
 import { upgradeWebSocket, websocket } from 'hono/bun'
 import type { ServerWebSocket } from 'bun'
 import { rateLimiter } from 'hono-rate-limiter'
+import { AlertEmail } from './utils/alert';
+
 
 
 const app = new Hono()
 const limiter = rateLimiter({
-  windowMs: 10 * 60 * 1000,
-  limit: 10,
-  standardHeaders: "draft-6",
-  skip: (c) => {
-   if (c.req.query("token") === Bun.env.token) {
-       return c.req.query("token") === Bun.env.token
-   }  
-   return c.req.header("Auth") === `Bearer${Bun.env.token}`
-  },
-  keyGenerator: (c) => c.req.header("cf-connecting-ip") || c.req.header("x-forwarded-for") || "quest"
+    windowMs: 10 * 60 * 1000,
+    limit: 10,
+    standardHeaders: "draft-6",
+    skip: (c) => {
+        if (c.req.query("token") === Bun.env.token) {
+            return c.req.query("token") === Bun.env.token
+        }
+        return c.req.header("Auth") === `Bearer${Bun.env.token}`
+    },
+    keyGenerator: (c) => c.req.header("cf-connecting-ip") || c.req.header("x-forwarded-for") || "quest"
 })
 
 let currentStatus = {
-  "status": "offline",
-  "file": "",
+    "status": "offline",
+    "file": "",
 }
 
 
@@ -29,61 +31,78 @@ app.use(limiter)
 
 const activeSockets = new Set<ServerWebSocket>();
 app.get('/ws', upgradeWebSocket((c) => {
-  const tokenRecibido = c.req.query("token");
-  const tokenEsperado = Bun.env.token;
-  const canWrite = tokenRecibido === tokenEsperado;
+    const tokenRecibido = c.req.query("token");
+    const tokenEsperado = Bun.env.token;
+    const canWrite = tokenRecibido === tokenEsperado;
 
-  console.log(`[AUTH] Recibido: "${tokenRecibido}" | Esperado: "${tokenEsperado}" | Match: ${canWrite}`);
+    console.log(`[AUTH] Recibido: "${tokenRecibido}" | Esperado: "${tokenEsperado}" | Match: ${canWrite}`);
 
-  return {
-    onOpen(_, ws) {
-      const srv = ws.raw as ServerWebSocket;
-      activeSockets.add(srv)
+    return {
+        onOpen(_, ws) {
+            const srv = ws.raw as ServerWebSocket;
+            activeSockets.add(srv)
 
-      ws.send(JSON.stringify(currentStatus));
-      console.log(canWrite ? "Neovim connected" : "Web client connected");
-    },
+            ws.send(JSON.stringify(currentStatus));
+            console.log(canWrite ? "Neovim connected" : "Web client connected");
+        },
 
-    onMessage(event, ws) {
-      if (!canWrite) return
+        onMessage(event, ws) {
+            if (!canWrite) return
 
-      try {
+            try {
 
-        const message = event.data.toString();
-        const newData = JSON.parse(message);
+                const message = event.data.toString();
+                const newData = JSON.parse(message);
 
-        if (!newData.status) return;
+                if (!newData.status) return;
 
-        if (newData.status) {
-          currentStatus = newData;
-          activeSockets.forEach((socket) => {
-            if (socket !== ws.raw) {
-              socket.send(message);
+                if (newData.status) {
+                    currentStatus = newData;
+                    activeSockets.forEach((socket) => {
+                        if (socket !== ws.raw) {
+                            socket.send(message);
+                        }
+                    });
+                }
+            } catch (e) {
+                console.error("Error parsers:", e);
             }
-          });
-        }
-      } catch (e) {
-        console.error("Error parsers:", e);
-      }
-    },
+        },
 
-    onClose: (_, ws) => {
-      const srv = ws.raw as ServerWebSocket;
-      activeSockets.delete(srv);
-      if (canWrite) {
-        console.log("Neovim disconnected - Status set to offline");
-      }
+        onClose: (_, ws) => {
+            const srv = ws.raw as ServerWebSocket;
+            activeSockets.delete(srv);
+            if (canWrite) {
+                console.log("Neovim disconnected - Status set to offline");
+            }
+        }
     }
-  }
 }))
+
+let heartbeatTimeout: Timer | null = null;
+app.post("/heartbeat", (c) => {
+    if (heartbeatTimeout) {
+        clearTimeout(heartbeatTimeout);
+        console.log("received signal");
+    }
+    heartbeatTimeout = setTimeout(async () => {
+        try {
+            console.log("no received signal")
+            await AlertEmail()
+        } catch (err) {
+            console.error("error", err)
+        }
+    }, 10 * 60 * 1000)
+    return c.text("ok")
+})
 
 
 app.get('/', (c) => {
-  return c.text('active uptimeRobot')
+    return c.text('active uptimeRobot')
 })
 
 export default {
-  port: process.env.PORT || 3000,
-  fetch: app.fetch,
-  websocket
+    port: process.env.PORT || 3000,
+    fetch: app.fetch,
+    websocket
 } 
---
+++ src/utils/alert.ts
@@ -0,0 +1,20 @@
+import { Resend } from "resend"
+
+const resend = new Resend(Bun.env.resend_token);
+
+
+const html = `
+<p>Alert Homelab is Down</p>
+<p>Reset now</p>
+`
+
+export const AlertEmail = async () => {
+    const email = Bun.env.email
+    if (!email) return;
+    await resend.emails.send({
+        from: "AlertEmail <support@nxbim.xyz>",
+        to:  email,
+        subject: "Alert Homelab Down",
+        html,
+    })
+}
